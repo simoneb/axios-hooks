1fccd27d9b5cc9c6a648e81e0656d6c2c383af81
Simone Busoli <simone.busoli@gmail.com>
2019-07-18 00:57:22 +0200

    feat: Allow using the cache when triggering requests manually
    Closes #19

--- index.d.ts
+++ index.d.ts
@@ -1,16 +1,23 @@
 import { AxiosRequestConfig, AxiosError, AxiosPromise } from 'axios'
 
 interface ResponseValues {
-  data: any;
-  loading: boolean;
-  error?: AxiosError;
+  data: any
+  loading: boolean
+  error?: AxiosError
 }
 
 interface Options {
   manual: boolean
 }
 
+interface RefetchOptions {
+  useCache: boolean
+}
+
 export default function useAxios(
   config: AxiosRequestConfig | string,
   options?: Options
-): [ResponseValues, (config?: AxiosRequestConfig) => void]
+): [
+  ResponseValues,
+  (config?: AxiosRequestConfig, options?: RefetchOptions) => void
+]
