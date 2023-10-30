927e87c4696b28cf6d41b127572a24def0d0f759
Ehsan Samavati <samaavaati@gmail.com>
2023-10-31 00:55:38 +0300

    test: migrate tests to typescript
    Remove JavaScript test files
    Convert remaining tests to TypeScript
    Update Jest config to only run TS tests
    Remove no longer needed type declaration files
    Update tests to properly mock axios as a typed mock
    Fix minor type errors in tests
    Add types to test utilities
    This finishes migration of the tests to TypeScript for improved compile time checking and takes full advantage of TypeScript in the tests.

--- test/testing-library__react-hooks.d.ts
+++
@@ -1,7 +0,0 @@
-import { RenderHookResult } from '@testing-library/react-hooks'
-
-declare module '@testing-library/react-hooks' {
-  interface RenderHookResult<P, R> {
-    readonly wait: (callback: Function, options: Object) => Promise<void>
-  }
-}
